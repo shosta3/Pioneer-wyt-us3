defa9b1d07d34d888faaf27f59c0bdc24fce69cc
shosta3 <126116033+shosta3@users.noreply.github.com>
2026-03-18 08:35:51 -0400

    Delete components/climate directory

--- components/climate/__init__.py
+++
@@ -1,5 +0,0 @@
-#import esphome.config_validation as cv
-#COMPONENT_SCHEMA = cv.Schema({})
-
-DEPENDENCIES = ["uart", "climate", "sensor", "switch", "select"]
-AUTO_LOAD = ["climate", "sensor", "switch", "select"]
